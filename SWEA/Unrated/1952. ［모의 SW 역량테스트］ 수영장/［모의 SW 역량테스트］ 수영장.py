83998951ea614c7ee62e91dbcd1e7b4796ad56ee
hyuna0810 <cindy0810.cho@gmail.com>
2025-04-21 14:41:45 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 62 ms, Memory: 51,328 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,34 +1,29 @@
-def recur(amount,month):
-    global final_amount
-    if final_amount < amount:
-        return
-
-    if month > 12:
-        final_amount = min(final_amount, amount)
-        return
-
-
-    # 1일 이용권을 구매하는 경우
-    recur(amount + (day_1 * arr[month]), month + 1)
-
-    # 1달 이용권을 구매하는 경우
-    recur(amount + month_1, month + 1)
-
-    # 3달 이용권을 구매하는 경우
-    recur(amount + month_3, month + 3)
-
-    # 1년 이용권을 구매하는 경우
-    recur(amount + year_1, month + 12)
-
-
-
+# 수영장
+# 아마도 DP?
 T=int(input())
 for tc in range(1,T+1):
-    day_1,month_1,month_3,year_1=map(int,input().split())
-    arr=[0]+list(map(int,input().split()))
+    day,month,three_month,year=map(int,input().split())
+    plan=[0]+list(map(int,input().split())) # 12개월 이용 계획
 
-    final_amount=int(21e9)
+    # 모든 경우의 수를 살펴보자
+    # 1. 1일 이용권 사용
+    # 2. 한달 이용권 사용
+    # 3. 3달 이용권 사용(현재 달 부터)
+    # 4. 1년 이용권 사용
 
-    recur(0, 1)
+    price=0 # 변수 초기화
+    dp=[0]*13 # 배열 생성 
     
-    print(f'#{tc} {final_amount}')
+    # bottom-up으로 가장 작은 문제로부터 큰 문제 구하기 
+    for i in range(1,13):
+        # 1. 현재 달을 1일권으로 할지 한달 권으로 할지
+        dp[i]=dp[i-1]+min(plan[i]*day,month)
+        # 2. 3달 이용권을 사용하는 경우
+        if i>=3:
+            # dp[i]=1일권, 1달권의 조합과 3달권을 비교
+            dp[i]=min(dp[i],dp[i-3]+three_month)
+        # 3. 1년 이용권 사용
+        if i==12:
+            dp[i]=min(dp[i],year)
+
+    print(f'#{tc} {dp[12]}')
